487528f9eb297a55884308f88ad5c15188f0deba
MinSeok Kim <70751423+MinSeok-dev@users.noreply.github.com>
2024-02-20 15:53:09 +0900

    [Bronze I] Title: 진법 변환 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -0,0 +1,13 @@
+N, B = map(int, input().split())
+
+result = ''
+
+while N > 0:
+    remainder = N % B
+    if remainder < 10:
+        result = str(remainder) + result
+    else:
+        result = chr(ord('A') + remainder - 10) + result
+    N //= B
+
+print(result if result else '0')
