765441a181e48017a126a64189ba9a47a7f86000
MinSeok Kim <70751423+MinSeok-dev@users.noreply.github.com>
2024-02-21 19:48:40 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -2,8 +2,10 @@ N, K = map(int, input().split())
 
 divisors = []
 
-for i in range(1, N+1):
+for i in range(1, int(N**0.5)+1):
     if N % i == 0:
         divisors.append(i)
-
+        if i != N // i:
+            divisors.append(N // i)
+divisors.sort()
 print(divisors[K-1] if K <= len(divisors) else 0)
