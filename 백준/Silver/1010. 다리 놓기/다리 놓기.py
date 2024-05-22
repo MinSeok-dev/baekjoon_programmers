fe66e816a6566385aaebda36e9e6b5eb81edcf72
MinSeok Kim <70751423+MinSeok-dev@users.noreply.github.com>
2024-05-22 10:48:42 +0900

    [Silver V] Title: 다리 놓기, Time: 76 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,13 +1,8 @@
-def factorial(n):
-    num = 1
-    for i in range(1, n+1):
-        num *= i
-    return num
-
+import math
 
 T = int(input())
 
 for _ in range(T):
     n, m = map(int, input().split())
-    bridge = factorial(m) // (factorial(n) * factorial(m - n))
+    bridge = math.factorial(m) // (math.factorial(n) * math.factorial(m - n))
     print(bridge)
