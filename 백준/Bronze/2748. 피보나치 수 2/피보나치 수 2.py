a77847499e0666b42a052b1b4a12b9fc2ec78126
MinSeok Kim <70751423+MinSeok-dev@users.noreply.github.com>
2025-04-21 23:33:30 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    memoization = [0,1]
+    for i in range(2, n+1):
+        memoization.append(memoization[i-2] + memoization[i-1])
+    return memoization[n]
+
+if __name__ == "__main__":
+    n = int(input())
+    answer = fibonacci(n)
+    print(answer)
