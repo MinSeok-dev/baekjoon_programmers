d52f6fc609ac88ea416e76fc85c5ac0152ed4a6f
MinSeok Kim <70751423+MinSeok-dev@users.noreply.github.com>
2025-04-27 20:42:58 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -2,18 +2,7 @@ while True:
     num = input()
     if num == "0":
         break
-    arr = [int(x) for x in num]
-    if len(num)%2 == 1:
-        half1 = arr[:len(num)//2]
-        half2 = arr[len(num)//2+1:][::-1]
-        if half1 == half2:
-            print("yes")
-        else:
-            print("no")
+    if num == num[::-1]:
+        print("yes")
     else:
-        half1 = arr[:len(num)//2]
-        half2 = arr[len(num)//2:][::-1]
-        if half1 == half2:
-            print("yes")
-        else:
-            print("no")
+        print("no")
